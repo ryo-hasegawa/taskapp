d0e9bb66afc95d26153ea1050ee4aa12d3b92242
長谷川良 <hase.industries@i.softbank.jp>
2018-07-26 09:48:08 +0900

    追加機能の実装開始

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RealmSwift
+import UserNotifications
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
@@ -19,7 +20,7 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     // 日付近い順\順でソート：降順
     // 以降内容をアップデートするとリスト内は自動的に更新される。
     var taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending: false)  // ←追加
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +28,7 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         tableView.delegate = self
         tableView.dataSource = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -72,14 +73,36 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             if editingStyle == .delete {
-            // データベースから削除する  // ←以降追加する
-            try! realm.write {
-                self.realm.delete(self.taskArray[indexPath.row])
-                tableView.deleteRows(at: [indexPath], with: .fade)
+                // データベースから削除する  // ←以降追加する
+                try! realm.write {
+                    self.realm.delete(self.taskArray[indexPath.row])
+                    tableView.deleteRows(at: [indexPath], with: .fade)
+                }
+            }
+        }
+        // 削除されたタスクを取得する
+        let task = self.taskArray[indexPath.row]
+        
+        // ローカル通知をキャンセルする
+        let center = UNUserNotificationCenter.current()
+        center.removePendingNotificationRequests(withIdentifiers: [String(task.id)])
+        
+        // データベースから削除する
+        try! realm.write {
+            self.realm.delete(task)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+        
+        // 未通知のローカル通知一覧をログ出力
+        center.getPendingNotificationRequests { (requests: [UNNotificationRequest]) in
+            for request in requests {
+                print("/---------------")
+                print(request)
+                print("---------------/")
             }
         }
     }
-    }
+    
     // segue で画面遷移するに呼ばれる
     override func prepare(for segue: UIStoryboardSegue, sender: Any?){
         let inputViewController:InputViewController = segue.destination as! InputViewController
@@ -104,7 +127,7 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         super.viewWillAppear(animated)
         tableView.reloadData()
     }
-
-
+    
+    
 }
 
